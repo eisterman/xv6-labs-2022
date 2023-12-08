00ec8fa3f9e8af1f986a172a0dc5262ec1189a22
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2023-12-08 15:56:31 +0100

    Modify uvmcopy to remap the memory and clear PTE_W

--- kernel/vm.c
+++ kernel/vm.c
@@ -308,7 +308,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -317,11 +316,8 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    *pte = *pte | !PTE_W;
+    if(mappages(new, i, PGSIZE, pa, flags | !PTE_W) != 0){
       goto err;
     }
   }
